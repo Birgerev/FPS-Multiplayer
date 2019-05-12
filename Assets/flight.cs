575b558b3ae800e299d59f13eb2984251fb8e3ad
Simon Orlander <simonorlander@outlook.com>
2019-05-12 15:50:51 +0200

    Huge Map Update
    Added:
    Animated propeler on plane
    Plane camera for later
    Tank camera for later
    Basic layout for Finland map
    Created shooting range and vehicle testing range
    Renamed map to DarkCity

--- Assets/flight.cs
+++ Assets/flight.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HitlerPlane : MonoBehaviour
+public class flight : MonoBehaviour
 {
-    public int Speed;
-    public int Upp; 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rigidbody lol = GetComponent<Rigidbody>();
-        lol.AddRelativeForce(new Vector3(0, Upp, Speed));
+        
     }
 }
