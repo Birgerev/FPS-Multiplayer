405e25a384636903628e515e582bf396b455f478
Birger Evansson <bibbevidio@gmail.com>
2019-05-14 19:48:44 +0200

    Project organization

---
+++ Assets/Script/net,gamemode/Gamemode.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Gamemode : NetworkBehaviour
+{
+    public virtual void Start()
+    {
+        if (isServer)
+            Rpc_onStart();
+    }
+
+    [ClientRpc]
+    public virtual void Rpc_onStart()
+    {
+        new Player("");
+    }
+
+    [ClientRpc]
+    public virtual void Rpc_OnPlayerKilled(int victim, int killer)
+    {
+
+    }
+
+    [ClientRpc]
+    public virtual void Rpc_OnPlayerDamage(int victim, int attacker)
+    {
+
+    }
+
+    [ClientRpc]
+    public virtual void Rpc_End()
+    {
+
+    }
+
+    public virtual void Win()
+    {
+
+    }
+
+    public virtual void Lose()
+    {
+
+    }
+}
+
+class Player
+{
+    public string name = "Mexico";
+
+    public List<int> players = new List<int>();
+
+
+    public GamePlayer(string name)
+    {
+        this.name = name;
+    }
+}
